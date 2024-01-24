9616d05c1f3ed00dc9452b9ade915f174ceb1241
helowitch <heloiseperat@gmail.com>
2024-01-24 19:55:42 +0100

    supp enregistrement données

--- script.js
+++ script.js
@@ -1,48 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Chargement des réponses sauvegardées au démarrage de la page
-  chargerReponses();
-
-  // Écouteur d'événement sur le bouton de résultats
-  const resultatButton = document.getElementById('resultatButton');
-  if (resultatButton) {
-    resultatButton.addEventListener('click', () => {
-      // Afficher les résultats et sauvegarder l'indication que les résultats sont affichés
-      localStorage.setItem('kongResultatsAffiches', 'true');
-      calculerPourcentage();
-      sauvegarderReponses();
-    });
-  }
-
-  // Écouteur d'événement sur le bouton de réinitialisation
-  const resetButton = document.getElementById('resetButton');
-  if (resetButton) {
-    resetButton.addEventListener('click', () => {
-      // Réinitialisation des réponses et sauvegarde
-      document.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
-        checkbox.checked = false;
-      });
-      localStorage.removeItem('kongResultatsAffiches');
-      calculerPourcentage();
-      sauvegarderReponses();
-    });
-  }
-
-  // Écouteur d'événement sur le changement de chaque case à cocher
-  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', () => {
-      // Sauvegarde des réponses à chaque changement
-      sauvegarderReponses();
-    });
-  });
-
-  // Afficher les résultats si déjà affichés
-  const resultatsAffiches = localStorage.getItem('kongResultatsAffiches') === 'true';
-  if (resultatsAffiches) {
-    calculerPourcentage();
-  }
-});
-
 function calculerPourcentage() {
   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
   const categories = {};
@@ -56,38 +11,39 @@ function calculerPourcentage() {
   const pourcentageGlobal = Math.ceil((totalDeCochees / checkboxes.length) * 100);
 
   const resultatGlobalElement = document.getElementById('resultatGlobal');
-  const resultatsCategorieElement = document.getElementById('resultatsCategorie');
-  
-  // Effacer le texte si les résultats ne sont pas encore affichés
-  resultatGlobalElement.textContent = ''; 
-  resultatsCategorieElement.innerHTML = '';
+  if (totalDeCochees > 0) {
+    resultatGlobalElement.textContent = `Tu es kong à ${pourcentageGlobal.toFixed(0)}%.`;
 
-  if (localStorage.getItem('kongResultatsAffiches')) {
-    if (totalDeCochees > 0) {
-      resultatGlobalElement.textContent = `Tu es kong à ${pourcentageGlobal.toFixed(0)}%.`;
+    const resultatsCategorieElement = document.getElementById('resultatsCategorie');
+    resultatsCategorieElement.innerHTML = '';
 
-      // Trouver les deux catégories principales
-      const nomsCategories = {
-        'A': 'Kong show',
-        'B': 'Kong des cavernes',
-        'C': 'Kong strong',
-        'D': 'Kong beauf',
-        'E': 'Kongpétiteur',
-        'F': 'Mécakong',
-      };
+    // Trouver les deux catégories principales
+    const nomsCategories = {
+      'A': 'Kong show',
+      'B': 'Kong des cavernes',
+      'C': 'Kong strong',
+      'D': 'Kong beauf',
+      'E': 'Kongpétiteur',
+      'F': 'Mécakong',
+    };
 
-      const categoriesTriees = Object.entries(categories).sort((a, b) => b[1] - a[1]);
-      const deuxCategoriesPrincipales = categoriesTriees.slice(0, 2).map(cat => nomsCategories[cat[0]]);
+    const categoriesTriees = Object.entries(categories).sort((a, b) => b[1] - a[1]);
+    const deuxCategoriesPrincipales = categoriesTriees.slice(0, 2).map(cat => nomsCategories[cat[0]]);
 
-      // Afficher les deux catégories principales
-      const phrasePrincipales = document.createElement('p');
-      phrasePrincipales.textContent = `À l'intérieur de toi, il y a deux principaux kongs : ${deuxCategoriesPrincipales.join(' et ')}.`;
-      resultatsCategorieElement.appendChild(phrasePrincipales);
+    // Afficher les deux catégories principales
+    const phrasePrincipales = document.createElement('p');
+    phrasePrincipales.textContent = `À l'intérieur de toi, il y a deux principaux kongs : ${deuxCategoriesPrincipales.join(' et ')}.`;
+    resultatsCategorieElement.appendChild(phrasePrincipales);
 
-      // Afficher le diagramme en cercle
-      afficherDiagramme(categories);
-    } else {
-      resultatGlobalElement.textContent = "Tu n'es pas kong du tout... quel modèle de pureté !";
+    // Afficher le diagramme en cercle
+    afficherDiagramme(categories);
+  } else {
+    resultatGlobalElement.textContent = "Tu n'es pas kong du tout... quel modèle de pureté !";
+    document.getElementById('resultatsCategorie').innerHTML = '';
+    const diagrammeElement = document.getElementById('diagramme');
+    if (diagrammeElement instanceof HTMLCanvasElement) {
+      const context = diagrammeElement.getContext('2d');
+      context.clearRect(0, 0, diagrammeElement.width, diagrammeElement.height);
     }
   }
 }
@@ -131,31 +87,4 @@ function getNomCategorie(categorie) {
   };
 
   return nomsCategories[categorie] || 'Inconnu';
-}
-
-function sauvegarderReponses() {
-  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  const reponses = {};
-
-  checkboxes.forEach(checkbox => {
-    const id = checkbox.getAttribute('id');
-    reponses[id] = checkbox.checked;
-  });
-
-  localStorage.setItem('kongReponses', JSON.stringify(reponses));
-}
-
-function chargerReponses() {
-  const reponses = localStorage.getItem('kongReponses');
-
-  if (reponses) {
-    const reponsesParsed = JSON.parse(reponses);
-
-    for (const id in reponsesParsed) {
-      const checkbox = document.getElementById(id);
-      if (checkbox) {
-        checkbox.checked = reponsesParsed[id];
-      }
-    }
-  }
 }
